75a291493cbbf027766401a303c9403e6926ab60
Tjeu Kayim <TjeuKayim@users.noreply.github.com>
2017-04-09 14:02:48 +0200

    ES6 -> JS

--- docs/assets/js/qr-code-generator.js
+++ docs/assets/js/qr-code-generator.js
@@ -1,10 +1,6 @@
----
----
-
-
 document.forms['get-qr'].onsubmit = function(e){
   e.preventDefault()
-  var get_value = x => encodeURIComponent(document.querySelector(x).value)
+  var get_value = function(x) {return encodeURIComponent(document.querySelector(x).value)}
   var ec = get_value('select[name="qr-ec"]')
   var data = get_value('textarea[name="qr-data"]')
   var url = 'https://tjeu.pythonanywhere.com/qr.svg?qr-ec=' + ec + '&qr-data=' + data
