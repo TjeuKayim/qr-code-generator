8cf2a961f85fff9935222982ad954c55cb2938c5
TjeuKayim <tjeu.kayim@gmail.com>
2017-04-09 15:52:55 +0200

    Code toegevoegd

--- docs/assets/js/qr-code-generator.js
+++ docs/assets/js/qr-code-generator.js
@@ -6,3 +6,29 @@ document.forms['get-qr'].onsubmit = function(e){
   var url = 'https://tjeu.pythonanywhere.com/qr.svg?qr-ec=' + ec + '&qr-data=' + data
   document.getElementById('qr-img').setAttribute('src', url)
 }
+
+Element.prototype.drag = function(){
+  var mousemove = function(e){ // document mousemove
+      this.style.left = (e.clientX - .5*this.offsetWidth)+'px'
+      this.style.top  = (e.clientY - .5*this.firstChild.offsetHeight)+'px'
+  }.bind(this)
+
+  var mouseup = function(e){ // document mouseup
+      document.removeEventListener('mousemove',mousemove)
+      document.removeEventListener('mouseup',mouseup)
+      document.body.style.cursor = ''
+  }.bind(this)
+
+  this.firstChild.addEventListener('mousedown',function(e){ // element mousedown
+      document.addEventListener('mousemove',mousemove)
+      document.addEventListener('mouseup',mouseup)
+      document.body.style.cursor = 'grabbing'
+      e.preventDefault()
+  }.bind(this))
+}
+
+function showDragMe() {
+  var dragMe = document.getElementById('drag-me')
+  dragMe.style.display = 'block'
+  dragMe.drag()
+}
